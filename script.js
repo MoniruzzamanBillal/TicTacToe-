6ca875994af8f6b281f82cd890da4ca3656d13b3
Billal <mdmoniruzzamanbillal@gmail.com>
2022-12-07 22:56:59 +0600

    first design prototype

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let cell = document.querySelectorAll(".cell");
+let box = document.querySelector(".box");
+
+
+let win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let turn = "o";
+
+
+
+
+
+
+
+
